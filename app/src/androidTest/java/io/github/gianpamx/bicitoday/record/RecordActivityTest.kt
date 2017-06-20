9266ec1eca59509878b899a1d6eb5cad2df1d291
Juan Villa <juan64@gmail.com>
2017-06-19 23:48:36 -0500

    first commit: Record Activity

---
+++ app/src/androidTest/java/io/github/gianpamx/bicitoday/record/RecordActivityTest.kt
@@ -0,0 +1,9 @@
+package io.github.gianpamx.bicitoday.record
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RecordActivityTest {
+    
+}
---
+++ app/src/main/java/io/github/gianpamx/bicitoday/record/RecordActivity.kt
@@ -0,0 +1,13 @@
+package io.github.gianpamx.bicitoday.record
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.gianpamx.bicitoday.R
+
+class RecordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.record_activity)
+    }
+}
