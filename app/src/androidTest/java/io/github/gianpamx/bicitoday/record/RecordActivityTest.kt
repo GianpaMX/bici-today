c0fcd2ede10a7a426c76cc4cf2e579f612bc74c4
Juan Villa <juan64@gmail.com>
2017-07-14 00:25:48 -0500

    refactor: Test

--- app/src/androidTest/java/io/github/gianpamx/bicitoday/record/RecordActivityTest.kt
+++ app/src/androidTest/java/io/github/gianpamx/bicitoday/record/RecordActivityTest.kt
@@ -2,15 +2,13 @@ package io.github.gianpamx.bicitoday.record
 
 
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import io.github.gianpamx.EspressoTestsMatchers
 import io.github.gianpamx.bicitoday.R
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,21 +16,13 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class RecordActivityTest {
 
-    @Rule
-    var mActivityTestRule = ActivityTestRule(RecordActivity::class.java)
-
-    var fab: ViewInteraction? = null
-
-    @Before
-    @Throws(Exception::class)
-    fun setUp() {
-        fab = onView(withId(R.id.recordFloatingActionButton))
-    }
+    @get:Rule
+    var activityTestRule = ActivityTestRule(RecordActivity::class.java)
 
     @Test
     fun recordActivityTest() {
-        fab!!.perform(click())
+        onView(withId(R.id.recordFloatingActionButton)).perform(click())
 
-        fab!!.check(matches(EspressoTestsMatchers.withDrawable(R.drawable.ic_pause_24dp)))
+        onView(withId(R.id.pauseFloatingActionButton)).check(ViewAssertions.matches(isDisplayed()))
     }
 }
