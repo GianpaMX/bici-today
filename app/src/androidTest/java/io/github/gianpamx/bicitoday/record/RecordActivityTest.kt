332fc091c68f6557b4832a6cad2c0089b89a2cf7
Juan Villa <juan64@gmail.com>
2017-07-14 00:27:42 -0500

    refactor: simplify with full import

--- app/src/androidTest/java/io/github/gianpamx/bicitoday/record/RecordActivityTest.kt
+++ app/src/androidTest/java/io/github/gianpamx/bicitoday/record/RecordActivityTest.kt
@@ -3,7 +3,7 @@ package io.github.gianpamx.bicitoday.record
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
@@ -23,6 +23,6 @@ class RecordActivityTest {
     fun recordActivityTest() {
         onView(withId(R.id.recordFloatingActionButton)).perform(click())
 
-        onView(withId(R.id.pauseFloatingActionButton)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.pauseFloatingActionButton)).check(matches(isDisplayed()))
     }
 }
